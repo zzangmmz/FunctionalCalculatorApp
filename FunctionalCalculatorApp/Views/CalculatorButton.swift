6397991a601dd23f4968c8991d73237f74dfd9bc
zzangmmz <leemj036@gmail.com>
2024-11-14 19:40:47 +0900

    feat: CalculatorButton 클래스 생성, UIColor extension으로 숫자 버튼 색상 정의

--- FunctionalCalculatorApp/Views/CalculatorButton.swift
+++ FunctionalCalculatorApp/Views/CalculatorButton.swift
@@ -1,1 +1,27 @@
+import UIKit
 
+final class CalculatorButton: UIButton {
+    init(_ title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = Int(title) != nil ? .numberColor : .orange
+        self.setTitle(title, for: .normal)
+        
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // 연산자, 피연산자 버튼 공통 속성
+    private func setup() {
+        self.titleLabel?.font = .boldSystemFont(ofSize: 30)
+        self.frame.size.height = 80
+        self.frame.size.width = 80
+        self.layer.cornerRadius = 40
+    }
+}
+
+extension UIColor {
+    static let numberColor = UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0)
+}
